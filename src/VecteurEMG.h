f42cc74f962a3c809157e42cb0e8d61bb096d108
Tristan Franc <Tristan19992010@gmail.com>
2021-03-11 17:40:24 -0500

    Revert "test"
    This reverts commit e45d5234f69903b65e3a7b8da76d8294c9049dc0.

--- src/VecteurEMG.h
+++ src/VecteurEMG.h
@@ -4,7 +4,7 @@
  *  Created on: 11 mars 2021
  *      Author: Justin
  */
-
+#include "VecteurEMG.h"
 
 #ifndef VECTEUREMG_H_
 #define VECTEUREMG_H_
--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,7 @@ int main(void) {
 	timerTest = new Timer(TIM4, 50000, false);
 	stm32F446->GPIO_Config(GPIOB,6,ALTERNATE,2);// met pbs en modee pwm
 	timerTest->enablePWM(1,200);
+	//timerTest->setPeriod(100);
 	timerTest->start();
 	while(1)
 	{
