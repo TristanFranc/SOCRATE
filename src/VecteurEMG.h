4de4a2d8274f7f7b2bed9054d8309ae8a7f4a447
jstnbelanger <80483870+jstnbelanger@users.noreply.github.com>
2021-03-11 17:05:40 -0500

    Update VecteurEMG.h
    a

--- src/VecteurEMG.h
+++ src/VecteurEMG.h
@@ -4,11 +4,13 @@
  *  Created on: 11 mars 2021
  *      Author: Justin
  */
+#include "VecteurEMG.h"
 
 #ifndef VECTEUREMG_H_
 #define VECTEUREMG_H_
 
-class VecteurEMG {
+class VecteurEMG
+{
 public:
 	VecteurEMG();
 	virtual ~VecteurEMG();
