31f95f6b72952cb40d24097e1fc227d74119688d
jstnbelanger <jstnbelanger52@gmeail.com>
2021-03-11 17:04:46 -0500

    Création classe

---
+++ src/VecteurEMG.h
@@ -0,0 +1,17 @@
+/*
+ * VecteurEMG.h
+ *
+ *  Created on: 11 mars 2021
+ *      Author: Justin
+ */
+
+#ifndef VECTEUREMG_H_
+#define VECTEUREMG_H_
+
+class VecteurEMG {
+public:
+	VecteurEMG();
+	virtual ~VecteurEMG();
+};
+
+#endif /* VECTEUREMG_H_ */
